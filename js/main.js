9c0af9472cef758315558d1d1c4a0ef6bc97a19c
mohammedgalal682@gmail.com <M.galal000>
2020-02-05 07:11:02 +0200

    ko

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(function () {
     this.left = posX - (this.width * 0.5);
   }
 
-  $(ripples).on('mousedown touchstart touch', function (e) {
+  $(ripples).on('mousedown', function (e) {
     //appending an element with a class name "btn-ripple"
 
     // $('.btn-ripple.active').remove();
@@ -33,7 +33,7 @@ $(function () {
   });
 
   //this event listener will be triggered once the ripple animation is done
-  $(ripples).on('mouseup blur mouseout touch', '.ripple-an', function () {
+  $(ripples).on('mouseup blur mouseout', '.ripple-an', function () {
     $(this).fadeOut(400, () => {
       $(this).remove()
     });
